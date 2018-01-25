3b2d40292fe2e4bd26ea44c92ae3b2f7969289eb
Peter Li <peterli@Peters-Mac-Pro.local>
2018-01-25 17:28:57 +0800

    Add script to install Chef version 12.5.1
    Also add steps to install docker, docker compose, nodejs, git, php
    in custom-script.sh.

--- custom-script.sh
+++ custom-script.sh
@@ -7,3 +7,22 @@ set -eux
 #
 # For example:
 # yum install -y curl wget git tmux firefox xvfb
+
+# Install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+
+# Install docker compose
+curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install nodejs, git, php
+echo "Add NodeSource Personal Package Archive and install nodejs"
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - && sudo apt-get update && sudo apt-get -y install nodejs
+echo "Install git"
+apt-get -y install git
+echo "Install PHP"
+apt-get -y install php-fpm php php-cli
